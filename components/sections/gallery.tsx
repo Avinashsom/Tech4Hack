68cf3ae18f45b428728755a54fbfb0ed28656fba
Avinash Som <avinash.som17@gmail.com>
2025-08-31 17:08:36 +0530

    w

--- components/sections/gallery.tsx
+++ components/sections/gallery.tsx
@@ -126,7 +126,7 @@ export function Gallery() {
         {/* Lightbox Modal */}
         {selectedImage && (
           <div className="fixed inset-0 z-50 bg-black/90 flex items-center justify-center p-4">
-            <div className="relative max-w-4xl max-h-full w-full max-w-full">
+            <div className="relative max-w-4xl max-h-full w-full">
               <button
                 onClick={() => setSelectedImage(null)}
                 className="absolute -top-12 right-0 text-white hover:text-gray-300 transition-colors"
