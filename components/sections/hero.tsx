4dbe7cfda8276ba2d6339fc8d3809cc1d4116a0c
Avinash Som <avinash.som17@gmail.com>
2025-09-03 14:25:05 +0530

    fix

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -25,7 +25,7 @@ export function Hero() {
 
   return (
     <section id="home"
-      className="min-h-screen flex items-center justify-center relative overflow-hidden pt-20 sm:pt-24 md:pt-28"
+      className="min-h-screen flex items-center justify-center relative overflow-hidden sm:pt-24 md:pt-28"
       >
 
     {/* className="min-h-screen flex items-center justify-center relative overflow-hidden" */}
