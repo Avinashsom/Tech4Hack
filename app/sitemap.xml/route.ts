c278cd6eecdc812f93763e42060acab2b90918b0
Avinash Som <avinash.som17@gmail.com>
2025-09-10 15:04:32 +0530

    site

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -1,6 +1,8 @@
 // app/sitemap.xml/route.ts
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const baseUrl = 'https://tech4hack.vercel.app';
 
